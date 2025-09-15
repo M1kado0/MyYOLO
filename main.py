5a5f81b98abebc6ba50e750f6eb7c3d9fa6c6695
M1kado0 <sagataldiyar@gmail.com>
2025-09-15 10:38:35 +0500

    created C2f and SPPF modules

--- main.py
+++ main.py
@@ -27,3 +27,46 @@ class Bottleneck(nn.Module):
             x += x_in
         return x
 
+
+class C2f(nn.Module):
+    def __init__(self, in_channels, out_channels, num_bottlenecks, shortcut=True):
+        super().__init__()
+        self.mid_channels = out_channels // 2
+        self.num_bottlenecks = num_bottlenecks
+        self.conv1 = Conv(in_channels, out_channels, kernel_size=1, stride=1, padding=0)
+        self.m = nn.Module([Bottleneck(self.mid_channels, self.mid_channels, shortcut) for _ in range(num_bottlenecks)])
+        self.conv2 = Conv(out_channels*(num_bottlenecks+2)//2, out_channels, kernel_size=1, stride=1, padding=0)
+        self.shortcut = shortcut
+    
+    def forward(self, x):
+        x = self.conv1(x)
+        x1, x2 = x[:, :x.shape[1]//2, :, :], x[:, x.shape[1]//2:, :, :]
+        outputs = [x1, x2]
+
+        for i in range(self.num_bottlenecks):
+            x1 = self.m[i](x1)
+            outputs = outputs.insert(0, x1)
+        outputs = torch.cat(outputs, dim=1)
+        out = self.conv2(outputs)
+        return out
+
+
+class SPPF(nn.Module):
+    def __init__(self, in_channels, out_channels, kernel_size=5):
+        super().__init__()
+        hidden_channels = in_channels // 2
+        self.conv1 = Conv(in_channels, hidden_channels, kernel_size=1, stride=1, padding=0)
+        self.pool = nn.MaxPool2d(kernel_size=kernel_size, stride=1, padding=kernel_size//2, dilation=1, ceil_mode=False)
+        self.conv2 = Conv(4*hidden_channels, out_channels, kernel_size=1, stride=1, padding=0)
+    
+    def forward(self, x):
+        x = self.conv1(x)
+
+        y1 = self.pool(x)
+        y2 = self.pool(y1)
+        y3 = self.pool(y2)
+
+        y = torch.cat([x,y1,y2,y3], dim=1)
+        
+        y = self.conv2(y)
+        return y
