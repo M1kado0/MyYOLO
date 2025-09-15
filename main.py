ff8eeb195ff431728085569b4ec6f0a31c5b4f20
M1kado0 <sagataldiyar@gmail.com>
2025-09-15 10:36:07 +0500

    created Conv and Bottleneck modules

--- main.py
+++ main.py
@@ -1,2 +1,29 @@
 import torch
 import torch.nn as nn
+
+class Conv(nn.Module):
+    def __init__(self, in_channels, out_channels, kernel_size, stride, padding):
+        super().__init__()
+        self.conv = nn.Conv2d(in_channels, out_channels, kernel_size, stride, padding)
+        self.bn = nn.BatchNorm2d(out_channels)
+        self.relu = nn.ReLU()
+    
+    def forward(self, x):
+        return self.relu(self.bn(self.conv(x)))
+
+
+class Bottleneck(nn.Module):
+    def __init__(self, in_channels, out_channels, shortcut=True):
+        super().__init__()
+        self.conv1 = Conv(in_channels, out_channels, kernel_size=3, stride=1, padding=1)
+        self.conv2 = Conv(out_channels, out_channels, kernel_size=3, stride=1, padding=1)
+        self.shortcut = shortcut
+
+    def forward(self, x):
+        x_in = x
+        x = self.conv1(x)
+        x = self.conv2(x)
+        if self.shortcut:
+            x += x_in
+        return x
+
